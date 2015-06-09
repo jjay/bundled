96bc58a0c6f6af75ba208290bae3cddb6e056495
Yakov Borevich <j.borevich@gmail.com>
2015-06-09 17:17:23 +0300

    Initial commit

---
+++ bundlesserver/uploader/views.py
@@ -0,0 +1,41 @@
+import logging
+import json
+from os import path, makedirs
+from django.http import HttpResponse, HttpResponseServerError
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+@csrf_exempt
+def upload(request):
+    try:
+        return unsafe_upload(request)
+    except Exception as e:
+        logging.exception("Bad thing happen:")
+        return HttpResponseServerError("Server Error: %s" %  e)
+        
+
+
+def unsafe_upload(request):
+    #print "Strt uploading, method:", request.method
+    #print request.body
+    #print request.POST["name"], "=", request.POST["version"]
+    #print request.FILES["data"]
+    versions = {}
+    versions_file = path.join(settings.DATA_PATH, "versions.json")
+    if path.exists(versions_file):
+        with open(versions_file, "r") as f:
+            versions = json.load(f)
+
+    versions[request.POST["name"]] = request.POST["version"]
+    with open(versions_file, "w") as f:
+        json.dump(versions, f)
+
+    targetfilename = path.join(settings.DATA_PATH, request.POST["name"])
+    if not path.exists(path.dirname(targetfilename)):
+        makedirs(path.dirname(targetfilename))
+    with open(targetfilename, "w") as targetfile:
+        for chunk in request.FILES["data"].chunks():
+            targetfile.write(chunk)
+    return HttpResponse("OK")
+
+# Create your views here.
