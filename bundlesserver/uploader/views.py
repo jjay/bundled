33267b7eaa9f6af4089ea81558a69b61a7723f6c
Yakov Borevich <j.borevich@gmail.com>
2015-08-04 16:22:53 +0300

    Support version

--- bundlesserver/uploader/views.py
+++ bundlesserver/uploader/views.py
@@ -20,8 +20,12 @@ def unsafe_upload(request):
     #print request.body
     #print request.POST["name"], "=", request.POST["version"]
     #print request.FILES["data"]
+    app_version = request.POST["app_version"]
+    home = path.join(settings.DATA_PATH, app_version)
+    if not path.exists(home):
+        mkdirs(home)
     versions = {}
-    versions_file = path.join(settings.DATA_PATH, "versions.json")
+    versions_file = path.join(home, "versions.json")
     if path.exists(versions_file):
         with open(versions_file, "r") as f:
             versions = json.load(f)
@@ -30,7 +34,7 @@ def unsafe_upload(request):
     with open(versions_file, "w") as f:
         json.dump(versions, f)
 
-    targetfilename = path.join(settings.DATA_PATH, request.POST["name"])
+    targetfilename = path.join(home, request.POST["name"])
     if not path.exists(path.dirname(targetfilename)):
         makedirs(path.dirname(targetfilename))
     with open(targetfilename, "w") as targetfile:
